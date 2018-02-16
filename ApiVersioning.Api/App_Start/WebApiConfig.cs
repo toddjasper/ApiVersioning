cb94997b669b90787839e2ecb7aea085a1e19c9a
Todd Jasper <todd.jasper@turn5.com>
2018-02-16 09:51:48 -0500

    Api works

--- ApiVersioning.Api/App_Start/WebApiConfig.cs
+++ ApiVersioning.Api/App_Start/WebApiConfig.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Web.Http;
-using Microsoft.Owin.Security.OAuth;
-using Newtonsoft.Json.Serialization;
+﻿using System.Web.Http;
 
 namespace ApiVersioning.Api
 {
@@ -15,7 +9,6 @@ namespace ApiVersioning.Api
             // Web API configuration and services
             // Configure Web API to use only bearer token authentication.
             config.SuppressDefaultHostAuthentication();
-            config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
 
             // Web API routes
             config.MapHttpAttributeRoutes();
---
+++ ApiVersioning.Api/Controllers/ProductController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace ApiVersioning.Api.Controllers
+{
+    public class ProductController : Controller
+    {
+        [HttpGet]
+        [Route("ajax/product")]
+        public JsonResult Index()
+        {
+            return Json(new {success = true}, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
